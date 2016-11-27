b42592e5d33d6a5fb32f7a16b9e66b13990d1257
Aleksandr Sher <aleksandrsher@gmail.com>
2016-11-27 09:17:14 +0200

    live demo; learning frontend

--- myo-sniff/src/myo_sniff/core.clj
+++ myo-sniff/src/myo_sniff/core.clj
@@ -5,7 +5,8 @@
             [clojure.string :as str]
             [clojure.core.async :as a]
 
-            [myo-sniff.events :as e]))
+            [myo-sniff.events :as e]
+            [myo-sniff.web :as web]))
 
 (defn foo
   "I don't do a whole lot."
@@ -44,13 +45,18 @@
 (def stream-emg->enabled (->command "set_stream_emg" {:type "enabled"}))
 (def locking-policy->none (->command "set_locking_policy" {:type "none"}))
 
+(defn socket
+  [on-receive]
+  (ws/connect
+   "ws://127.0.0.1:10138/myo/3"
+   :on-receive on-receive))
+
 (defn start!
   [folder id]
   (let [file (str "../samples/" folder "/" id ".log")
         _ (open-writer! file false)
         [input-ch _] (e/start-consumer)
-        socket (ws/connect "ws://127.0.0.1:10138/myo/3"
-                           :on-receive (put-file))]
+        socket (socket (put-file))]
     ;;(ws/send-msg socket locking-policy->none)
     (ws/send-msg socket stream-emg->enabled)
     [socket input-ch]))
@@ -65,3 +71,21 @@
 (comment (let [token (start! "seq" "r-b-c-a-5s-ilya-2")]
    (Thread/sleep (* 20 1000))
    (stop! token)))
+
+(defn start-web!
+  []
+  (let [[input-ch out-ch] (e/start-consumer)
+        server (web/websocket-consumer out-ch)
+        socket (socket (put-chan input-ch))]
+    (ws/send-msg socket stream-emg->enabled)
+    [socket input-ch]))
+
+(defn stop-web!
+  [[socket input-ch]]
+  (ws/close socket)
+  (Thread/sleep 100)
+  (a/close! input-ch))
+
+'(let [token (start-web!)]
+  (Thread/sleep (* 20 1000))
+  (stop! token))
